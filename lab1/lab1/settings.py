34d31fdaf12d8eab455de121782e6fb14e631a30
ADADadsdav <lilu.i.polina.strela@gmail.com>
2026-03-27 13:02:44 +0300

    авпп

--- lab1/lab1/settings.py
+++ lab1/lab1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ff+c553l!j#cdy!=7tz28^2tx@19g5k&da@djkoel#u0$l#oeo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'movies.apps.MoviesConfig',
 ]
 
 MIDDLEWARE = [
